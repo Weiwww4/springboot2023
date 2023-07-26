f3417017f616503b5f5f9d0a4553dbfd60f2ed1b
sazanka <ninjasazanka@gmail.com>
2023-07-26 23:03:55 +0800

    Project Init...

---
+++ record_test/src/main/java/com/faintdream/record/test/Test1.java
@@ -0,0 +1,8 @@
+package com.faintdream.record.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("First Project!!!");
+        System.out.println("Spring Boot!!!");
+    }
+}
